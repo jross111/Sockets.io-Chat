342e2270aa9643a7d1bac3fd5c2928495747623e
jross111 <jross.nyc111@gmail.com>
2020-03-26 17:29:33 -0400

    :rocket:

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import { join } from 'path';
-import http from 'http';
-import express, { static } from 'express';
+const path = require('path');
+const http = require('http');
+const express = require('express');
 
 const app = express();
+const server = http.createServer(app);
 
 // Set static folder
-app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 app.listen();
