769eb882f949c949b714e2604f041d6432906529
jross111 <jross.nyc111@gmail.com>
2020-03-27 00:13:15 -0400

    :checkered_flag:

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when FE connects
+
+io.on('connection', (socket) => {
+	console.log('Connection Made');
+});
 
 const PORT = 3000 || process.env.PORT;
 app.listen();
