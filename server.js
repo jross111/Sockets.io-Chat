bf682541eb27ec525f708e0afbcae6c58abe1e20
jross111 <jross.nyc111@gmail.com>
2020-03-27 20:23:32 -0400

    :zap:

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import { join } from 'path';
-import { createServer } from 'http';
-import express, { static } from 'express';
-import socketio from 'socket.io';
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
-const server = createServer(app);
+const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(static(join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when FE connects
 
